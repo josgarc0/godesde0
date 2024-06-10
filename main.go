728a1675a5b8b4196dcdc62423e9c2e65dfcf1bb
josgarc0 <jgarciaguti0@gmail.com>
2024-06-10 19:48:58 +0200

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/josgarc0/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoaTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
